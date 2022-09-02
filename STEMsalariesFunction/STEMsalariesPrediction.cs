85dcf382f90c4d26897007eec63749ee17a10dbc
Piova <ppiova@gmail.com>
2022-09-02 01:19:23 -0300

    AzureFunctions

---
+++ STEMsalariesFunction/STEMsalariesPrediction.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.ML;
+
+namespace STEMsalariesFunction
+{
+    public static class STEMsalariesPrediction
+        
+    {
+        [FunctionName("STEMsalariesPredictionAPI")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var taxiTrip = JsonConvert.DeserializeObject<Salary>(requestBody);
+
+
+            bool isLocal = true; // set to false before publishing to Azure
+
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = isLocal
+                ? LoadModelLocal(mlContext)
+                : LoadModelAzure(mlContext);
+
+            var predictionEnginePool = mlContext.Model
+                .CreatePredictionEngine<Salary, SalaryPrediction>(mlModel);
+
+            var prediction = predictionEnginePool.Predict(taxiTrip);
+
+            return new OkObjectResult(prediction);
+
+        }
+        static readonly string MLModelFileName = "MLModel.zip";
+
+        static readonly string MLModelPath =
+            Path.Combine(Environment.CurrentDirectory, MLModelFileName);
+
+        private static ITransformer LoadModelLocal(MLContext mlContext)
+        {
+            return mlContext.Model.Load(MLModelPath, out _);
+        }
+
+        private static ITransformer LoadModelAzure(MLContext mlContext)
+        {
+            // Only when publishing to Azure
+            var fileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            var path = fileInfo.Directory.Parent.FullName;
+            var mlModelPath = Path.Combine(path, MLModelFileName);
+
+            return mlContext.Model.Load(mlModelPath, out _);
+        }
+
+    }
+}
---
+++ STEMsalariesFunction/Salary.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ML.Data;
+
+namespace STEMsalariesFunction
+{
+    public class Salary
+    {
+        [ColumnName("Label"), LoadColumn(0)]
+        public float AnualCompensation { get; set; }
+
+        [LoadColumn(1)]
+        public string Company { get; set; }
+
+        [LoadColumn(2)]
+        public string Title { get; set; }
+
+        [LoadColumn(3)]
+        public float YearsExperience { get; set; }
+
+        [LoadColumn(4)]
+        public float YearsCompany { get; set; }
+
+        [LoadColumn(5)]
+        public string Gender { get; set; }
+
+        [LoadColumn(6)]
+        public string Location { get; set; }
+
+        [LoadColumn(7)]
+        public string Race { get; set; }
+
+        [LoadColumn(8)]
+        public string Education { get; set; }
+    }
+}
